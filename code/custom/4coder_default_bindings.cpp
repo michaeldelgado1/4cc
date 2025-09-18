c97f0bf306773a75950b1cc9bed449696aa01718
Michael Delgado <michaeldelgado1@gmail.com>
2025-09-17 19:14:42 -0700

    The very start of my vim mappings

--- code/custom/4coder_default_bindings.cpp
+++ code/custom/4coder_default_bindings.cpp
@@ -13,6 +13,43 @@
 #include "generated/managed_id_metadata.cpp"
 #endif
 
+String_ID mapid_shared;
+String_ID mapid_normal;
+String_ID mapid_insert;
+String_ID mapid_visual;
+
+void
+set_current_mapid(Application_Links *app, Command_Map_ID mapid) {
+  View_ID view = get_active_view(app, 0);
+  Buffer_ID buffer = view_get_buffer(app, view, 0);
+  Managed_Scope scope = buffer_get_managed_scope(app, buffer);
+  Command_Map_ID *map_id_ptr = scope_attachment(app, scope, buffer_map_id, Command_Map_ID);
+  *map_id_ptr = mapid;
+}
+
+CUSTOM_COMMAND_SIG(go_to_normal_mode) {
+  set_current_mapid(app, mapid_normal);
+
+  active_color_table.arrays[ defcolor_cursor ].vals[ 0 ] = 0xffff5533;
+  active_color_table.arrays[ defcolor_at_cursor ].vals[ 0 ] = 0xff00aacc;
+  active_color_table.arrays[ defcolor_margin_active ].vals[ 0 ] = 0xffff5533;
+}
+
+CUSTOM_COMMAND_SIG(go_to_insert_mode) {
+  set_current_mapid(app, mapid_insert);
+
+  active_color_table.arrays[ defcolor_cursor ].vals[ 0 ] = 0xff80ff80;
+  active_color_table.arrays[ defcolor_at_cursor ].vals[ 0 ] = 0xff293134;
+  active_color_table.arrays[ defcolor_margin_active ].vals[ 0 ] = 0xff80ff80;
+}
+
+CUSTOM_COMMAND_SIG(go_to_visual_mode) {
+  set_current_mapid(app, mapid_visual);
+
+  active_color_table.arrays[ defcolor_cursor ].vals[ 0 ] = 0xffffff00;
+  active_color_table.arrays[ defcolor_at_cursor ].vals[ 0 ] = 0xff0000ff;
+}
+
 // TODO(mdelgado): I'd like to avoid updating this function,
 //  I don't think this system gives me that ability to define
 //  this in my 4coder_custom.cpp file
@@ -29,6 +66,12 @@ custom_layer_init(Application_Links *app){
   String_ID global_map_id = vars_save_string_lit("keys_global");
   String_ID file_map_id = vars_save_string_lit("keys_file");
   String_ID code_map_id = vars_save_string_lit("keys_code");
+
+  mapid_shared = vars_save_string_lit("mapid_shared");
+  mapid_normal = vars_save_string_lit("mapid_normal");
+  mapid_insert = vars_save_string_lit("mapid_insert");
+  mapid_visual = vars_save_string_lit("mapid_visual");
+
 #if OS_MAC
     setup_mac_mapping(&framework_mapping, global_map_id, file_map_id, code_map_id);
 #else
@@ -39,29 +82,52 @@ custom_layer_init(Application_Links *app){
   MappingScope();
   SelectMapping(&framework_mapping);
 
-  SelectMap(global_map_id);
-  Bind(exit_4coder, KeyCode_Q, KeyCode_Control);
+
+  SelectMap(mapid_shared);
+  BindCore(default_startup, CoreCode_Startup);
+  BindCore(default_try_exit, CoreCode_TryExit);
+  Bind(go_to_normal_mode, KeyCode_Escape);
   Bind(change_active_panel, KeyCode_L, KeyCode_Control);
   Bind(change_active_panel_backwards, KeyCode_H, KeyCode_Control);
+  Bind(exit_4coder, KeyCode_Q, KeyCode_Control);
+  Bind(interactive_open_or_new, KeyCode_O, KeyCode_Control);
+  Bind(quick_swap_buffer, KeyCode_6, KeyCode_Control);
+  /*
+   * TODO(mdelgado): Make the below commented out code work. I'd like to
+   *  be able to go to the next suggestion by pressing ctrl-n and previous
+   *  by pressing ctrl-p. Right now that looks like I'd have to change the
+   *  actual implementation.
+   *  Bind(word_complete_drop_down,    KeyCode_Space, KeyCode_Control);
+   */
+  Bind(word_complete, KeyCode_Space, KeyCode_Control);
+
+  SelectMap(mapid_normal);
+  ParentMap(mapid_shared);
+  Bind(redo, KeyCode_R, KeyCode_Control);
+  Bind(undo, KeyCode_U);
+  Bind(move_down, KeyCode_J);
+  Bind(move_up, KeyCode_K);
+  Bind(move_left, KeyCode_H);
+  Bind(move_right, KeyCode_L);
+  Bind(go_to_insert_mode, KeyCode_I);
+  Bind(delete_char, KeyCode_X);
+
+  SelectMap(mapid_insert);
+  ParentMap(mapid_shared);
+  BindTextInput(write_text_and_auto_indent);
+  Bind(delete_char, KeyCode_Delete);
 
   SelectMap(file_map_id);
-  ParentMap(global_map_id);
-  /* NOTE(mdelgado): I don't love that I have to do this on both
-      a global_map and file map. Since ctrl-q seems to be set in
-      the file_map my quit map will get consumed by the file version,
-      When we fully reset these bindings, I may not have to do this. */
-  Bind(exit_4coder, KeyCode_Q, KeyCode_Control);
-  // NOTE(mdelgado): Also pagued by having a global binding and file binding
-  Bind(change_active_panel, KeyCode_L, KeyCode_Control);
+  ParentMap(mapid_normal);
 
-  /* TODO(mdelgado): Make the below commented out code work. I'd like to
-      be able to go to the next suggestion by pressing ctrl-n and previous
-      by pressing ctrl-p. Right now that looks like I'd have to change the
-      actual implementation.
-     Bind(word_complete_drop_down,    KeyCode_Space, KeyCode_Control); */
-  Bind(word_complete, KeyCode_Space, KeyCode_Control);
-  Bind(redo, KeyCode_R, KeyCode_Control);
-  Bind(quick_swap_buffer, KeyCode_6, KeyCode_Control);
+  SelectMap(code_map_id);
+  ParentMap(mapid_normal);
+
+  // TODO(mdelgado): Since we start in normal mode, we should indicate it.
+  //  I'll most likely need to do this in a hook
+  // active_color_table.arrays[ defcolor_cursor ].vals[ 0 ] = 0xffff5533;
+  // active_color_table.arrays[ defcolor_at_cursor ].vals[ 0 ] = 0xff00aacc;
+  // active_color_table.arrays[ defcolor_margin_active ].vals[ 0 ] = 0xffff5533;
 }
 
 #endif //FCODER_DEFAULT_BINDINGS
